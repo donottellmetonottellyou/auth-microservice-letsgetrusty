eb148d597c59507432930886dd20d16c619aeda3
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-10-25 15:39:22 -0400

    finished client/main

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,16 +1,14 @@
-use std::env;
-use clap::{Parser, Subcommand};
-
-use authentication::auth_client::AuthClient;
-use authentication::{SignInRequest, SignOutRequest, SignUpRequest};
-use tonic::transport::Channel;
-use tonic::{Request, Response};
-
-use crate::authentication::{SignUpResponse, SignInResponse, SignOutResponse};
-
 pub mod authentication {
     tonic::include_proto!("authentication");
 }
+
+use crate::authentication::{SignInResponse, SignOutResponse, SignUpResponse};
+use authentication::{auth_client::AuthClient, SignInRequest, SignOutRequest, SignUpRequest};
+
+use clap::{Parser, Subcommand};
+use tonic::{transport::Channel, Request};
+
+use std::env;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -19,6 +17,7 @@ struct Cli {
     command: Option<Commands>,
 }
 
+#[allow(clippy::enum_variant_names)]
 #[derive(Subcommand)]
 enum Commands {
     SignIn {
@@ -43,32 +42,43 @@ enum Commands {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // AUTH_SERVICE_IP can be set to your droplet's ip address once your app is deployed
     let auth_ip = env::var("AUTH_SERVICE_IP").unwrap_or("[::0]".to_owned());
-    let mut client: AuthClient<Channel> = todo!(); // Create new `AuthClient` instance. Propagate any errors.
+    let mut client: AuthClient<Channel> =
+        AuthClient::connect(format!("http://{auth_ip}:50051")).await?;
 
     let cli = Cli::parse();
 
     match &cli.command {
         Some(Commands::SignIn { username, password }) => {
-            let request: Request<SignInRequest> = todo!(); // Create a new `SignInRequest`.
-        
-            // Make a sign in request. Propagate any errors. Convert Response<SignInResponse> into SignInResponse.
-            let response: SignInResponse = todo!();
-        
-            println!("{:?}", response);
+            let response: SignInResponse = client
+                .sign_in(Request::new(SignInRequest {
+                    username: username.clone(),
+                    password: password.clone(),
+                }))
+                .await?
+                .into_inner();
+
+            println!("{response:?}");
         }
         Some(Commands::SignUp { username, password }) => {
-            let request: Request<SignUpRequest> = todo!(); // Create a new `SignUpRequest`.
-        
-            let response: Response<SignUpResponse> = todo!(); // Make a sign up request. Propagate any errors.
-        
-            println!("{:?}", response.into_inner());
+            let response: SignUpResponse = client
+                .sign_up(Request::new(SignUpRequest {
+                    username: username.clone(),
+                    password: password.clone(),
+                }))
+                .await?
+                .into_inner();
+
+            println!("{response:?}");
         }
         Some(Commands::SignOut { session_token }) => {
-            let request: Request<SignOutRequest> = todo!(); // Create a new `SignOutRequest`.
-        
-            let response: Response<SignOutResponse> = todo!(); // Make a sign out request. Propagate any errors.
-        
-            println!("{:?}", response.into_inner());
+            let response: SignOutResponse = client
+                .sign_out(Request::new(SignOutRequest {
+                    session_token: session_token.clone(),
+                }))
+                .await?
+                .into_inner();
+
+            println!("{response:?}");
         }
         None => {}
     }
